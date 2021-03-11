e5ca4c546ba0cb7009fabd6e9cc1c891a948ccdc
Quyen <quyen.wichers@sjsu.edu>
2021-03-10 23:22:54 -0800

    Fix bug

--- app/src/main/java/com/sjsu/partyplanner/DetailActivity.java
+++ app/src/main/java/com/sjsu/partyplanner/DetailActivity.java
@@ -13,26 +13,22 @@ import com.sjsu.partyplanner.databinding.ActivityDetailBinding;
 
 public class DetailActivity extends AppCompatActivity {
   private ActivityDetailBinding detailBinding;
-
+  private FragmentTransaction fragmentTransaction;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     detailBinding = ActivityDetailBinding.inflate(getLayoutInflater());
     View view = detailBinding.getRoot();
     setContentView(view);
-
+    fragmentTransaction = getSupportFragmentManager().beginTransaction();
   }
 
   public void showGuests(View view){
-//    Intent intent = new Intent(this, DetailFragment.class);
-////    intent.putExtra(DETAIL_MESSAGE, selectedItem);
-//    startActivity(intent);
     // Begin the transaction
-    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-// Replace the contents of the container with the new fragment
-    ft.replace(R.id.eventDetailDisplay, new GuestFragment());
-// or ft.add(R.id.your_placeholder, new FooFragment());
-// Complete the changes added above
-    ft.commit();
+    // Replace the contents of the container with the new fragment
+    fragmentTransaction.replace(R.id.eventDetailDisplay, new GuestFragment());
+    // or ft.add(R.id.your_placeholder, new FooFragment());
+    // Complete the changes added above
+    fragmentTransaction.commit();
   }
 }
