9f874dc85f7b12d70d336f37fda43784a513525f
Julie Loi <36089473+JLoiLeaf@users.noreply.github.com>
2021-05-13 15:46:11 -0700

    Fixed Merge

--- app/src/main/java/com/sjsu/partyplanner/Activities/Parties/PartyDetailActivity.java
+++ app/src/main/java/com/sjsu/partyplanner/Activities/Parties/PartyDetailActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
